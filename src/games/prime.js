06ff1826ae82c92b09c83cfd426bafd34ef00c02
blake <qblake.public@gmail.com>
2018-09-16 16:56:00 +0400

    refactor: make isPrime recursive

--- src/games/prime.js
+++ src/games/prime.js
@@ -2,11 +2,21 @@ import { cons } from 'hexlet-pairs';
 import getRandomNumber from '../utils/utils';
 import { runGame } from '..';
 
-const isPrime = (num) => {
-  for (let i = 2; i < num; i += 1) {
-    if (num % i === 0) return false;
+const hasDivisorWithZeroRemainder = (number, divisor) => {
+  if (divisor === 1) {
+    return false;
   }
-  return num !== 1 && num !== 0;
+  if (number % divisor === 0) {
+    return true;
+  }
+  return hasDivisorWithZeroRemainder(number, divisor - 1);
+};
+
+const isPrime = (number) => {
+  if (number < 2) {
+    return false;
+  }
+  return !hasDivisorWithZeroRemainder(number, Math.floor(number / 2));
 };
 
 const primeGame = () => ({
